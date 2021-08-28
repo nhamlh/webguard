85241d364a2fdbdc071005713d1027b03b69103b
Nham Le <lehoainham@gmail.com>
2021-08-28 14:27:26 +0700

    Proper handling db seed cmd

--- cmd/db.go
+++ cmd/db.go
@@ -1,12 +1,16 @@
 package main
 
 import (
+	"bytes"
+	"html/template"
+	"log"
+
 	"github.com/golang-migrate/migrate/v4"
 	"github.com/golang-migrate/migrate/v4/database/sqlite3"
 	_ "github.com/golang-migrate/migrate/v4/source/file"
 	"github.com/nhamlh/wg-dash/pkg/db"
 	"github.com/spf13/cobra"
-	"log"
+	"golang.org/x/crypto/bcrypt"
 )
 
 func newDbCmd() *cobra.Command {
@@ -17,6 +21,7 @@ func newDbCmd() *cobra.Command {
 	}
 
 	cmd.AddCommand(newDBMigrateCmd())
+	cmd.AddCommand(newDBSeedCmd())
 
 	return cmd
 }
@@ -37,6 +42,43 @@ func newDBMigrateCmd() *cobra.Command {
 			}
 
 			m.Steps(2)
+		},
+	}
+
+	return cmd
+}
+
+func newDBSeedCmd() *cobra.Command {
+	cmd := &cobra.Command{
+		Use:   "seed",
+		Short: "Database data seeding",
+		Run: func(cmd *cobra.Command, args []string) {
+			tmpl, _ := template.New("sql").Parse(`
+insert into users(email,password,is_admin,auth_type)
+values("nham", "{{ $.password }}", 1, 0)
+on conflict do nothing;
+
+insert into devices(user_id,name,private_key,allowed_ips)
+values(1, "one", "ek0si+w7pu2uomyue83/dj6xnvrgrovacvpblvowev0=", "0.0.0.0/0");
+insert into devices(user_id,name,private_key,allowed_ips)
+values(1, "two", "gkm16zjyrnay5ojsewlqx7fxu6dyszwmresjofq2dw8=", "1.1.1.1/1");
+
+insert into users(email,password,is_admin,auth_type)
+values("test", "{{ $.password }}", 1, 0)
+on conflict do nothing;
+
+insert into devices(user_id,name,private_key,allowed_ips)
+values(2, "four", "8o9jcfjask2zuvtc77sd2nwzvnil3loibickg3z3238=", "0.0.0.0/0");
+`)
+			password, err := bcrypt.GenerateFromPassword([]byte("abc"), bcrypt.DefaultCost)
+			if err != nil {
+				log.Fatal(err)
+			}
+
+			sql := bytes.NewBufferString("")
+			tmpl.Execute(sql, map[string]interface{}{"password": string(password)})
+
+			db.DB.MustExec(sql.String())
 		},
 	}
 
--- pkg/db/db.go
+++ pkg/db/db.go
@@ -3,6 +3,7 @@ package db
 import (
 	"github.com/jmoiron/sqlx"
 	_ "github.com/mattn/go-sqlite3"
+	// "github.com/nhamlh/wg-dash/pkg/config"
 )
 
 var DB *sqlx.DB
@@ -22,25 +23,23 @@ func initDb() *sqlx.DB {
 	return db
 }
 
-func MigrateSchema() {
-	DB.MustExec(schema)
+// func migrateschema() {
+// 	// seeding
+// 	db.mustexec(`
+// insert into users(email,password,is_admin,auth_type)
+// values("nham", "$2a$14$vxnb2arwqj0eueo.1g25yotnga/9amsxaehx5hnxpddszat1coob2", 1, 0)
+// on conflict do nothing;
 
-	// Seeding
-	DB.MustExec(`
-INSERT INTO users(email,password,is_admin,auth_type)
-values("nham", "$2a$14$VxNB2aRwQj0eueo.1g25YOtnga/9AmSxAeHX5hnXpdDszat1COob2", 1, 0)
-ON CONFLICT DO NOTHING;
+// insert into devices(user_id,name,private_key,allowed_ips)
+// values(1, "one", "ek0si+w7pu2uomyue83/dj6xnvrgrovacvpblvowev0=", "0.0.0.0/0");
+// insert into devices(user_id,name,private_key,allowed_ips)
+// values(1, "two", "gkm16zjyrnay5ojsewlqx7fxu6dyszwmresjofq2dw8=", "1.1.1.1/1");
 
-INSERT INTO devices(user_id,name,private_key,allowed_ips)
-values(1, "one", "EK0si+W7Pu2UoMYUE83/Dj6XNVrgROVACVpBlVOwEV0=", "0.0.0.0/0");
-INSERT INTO devices(user_id,name,private_key,allowed_ips)
-values(1, "two", "gKM16ZjyrnAY5OjsEwLqX7FXu6DySZwmrEsjOFq2DW8=", "1.1.1.1/1");
+// insert into users(email,password,is_admin,auth_type)
+// values("test", "$2a$14$vxnb2arwqj0eueo.1g25yotnga/9amsxaehx5hnxpddszat1coob2", 1, 0)
+// on conflict do nothing;
 
-INSERT INTO users(email,password,is_admin,auth_type)
-values("test", "$2a$14$VxNB2aRwQj0eueo.1g25YOtnga/9AmSxAeHX5hnXpdDszat1COob2", 1, 0)
-ON CONFLICT DO NOTHING;
-
-INSERT INTO devices(user_id,name,private_key,allowed_ips)
-values(2, "four", "8O9JcFJasK2zUvtC77sD2nWzVnIl3lOibIcKG3Z3238=", "0.0.0.0/0");
-`)
-}
+// insert into devices(user_id,name,private_key,allowed_ips)
+// values(2, "four", "8o9jcfjask2zuvtc77sd2nwzvnil3loibickg3z3238=", "0.0.0.0/0");
+// `)
+// }
